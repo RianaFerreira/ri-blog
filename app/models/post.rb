cf0ec8ac9509ecb7d84337029d4a154e5da2118f
RianaFerreira <rianaferreira@gmail.com>
2014-02-23 14:38:49 +1100

    working on the update error - private method 'update' called for the post being edited

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,7 @@
 #
 
 class Post < ActiveRecord::Base
-  attr_accessible :detail, :image, :title
+  attr_accessible :title, :detail, :image
 
   validates :title, :detail, presence: true,
                     length: { minimum: 5 }
