f08d099a8844e0c10fba8551e8daa43f132853bd
RianaFerreira <rianaferreira@gmail.com>
2014-03-08 19:29:59 +1100

    seed data for posts and access features reviewed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'faker'
+puts "~ Deleting all posts and associated comments"
+Post.delete_all
 
+puts "~ Creating and populating 20 posts and comments"
 20.times do |i|
-  Post.create!(title: Faker::Lorem.sentence, detail: Faker::Lorem.paragraph )
+  @post = Post.create!( title: Faker::Lorem.sentence, detail: Faker::Lorem.paragraph )
+  @post.comments.create!(commenter: Faker::Name.name, body: Faker::Lorem.paragraph )
 end
+
+puts "~ Database seeded successfully"
