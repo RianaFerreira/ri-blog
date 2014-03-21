9d40b8b00be1434d0f3cbd3e92fee6c045bb122a
RianaFerreira <rianaferreira@gmail.com>
2014-03-22 10:23:42 +1100

    changes related to the thought attribute on posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,13 +8,14 @@
 #  image      :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  thought    :string(255)
 #
 
 class Post < ActiveRecord::Base
   # enable elastic search on posts
   searchkick
 
-  attr_accessible :title, :detail, :image, :tags_attributes
+  attr_accessible :title, :detail, :thought, :image, :tags_attributes
 
   # post validations
   validates :title, presence: true, length: { minimum: 5 }
@@ -27,5 +28,14 @@ class Post < ActiveRecord::Base
   # macro to edit tags via posts with nested attributes
   accepts_nested_attributes_for :tags, allow_destroy: true,
                                 :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+
+  # search query
+  def self.search(query)
+    if query
+      find(:all, :conditions => ['detail LIKE ?', "%#{query}%"])
+    else
+      find(:all)
+    end
+  end
 
 end
