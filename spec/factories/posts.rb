3eaee637087b36ba797db7e02211c6739feadbc0
RianaFerreira <rianaferreira@gmail.com>
2014-03-22 14:14:20 +1100

    rspec tests using FactoryGirl for post model

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -12,11 +12,13 @@
 #
 
 # Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
 
 FactoryGirl.define do
-  factory :post do
-    title "MyString"
-    detail "MyText"
-    image "MyString"
+  factory :post do |f|
+    f.title {Faker::Lorem.sentence}
+    f.detail {Faker::Lorem.paragraph}
+    f.thought {Faker::Lorem.sentence}
+    f.image "MyString"
   end
 end
